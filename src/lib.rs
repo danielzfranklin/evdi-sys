29399e8968fe7ae9ca3c018f50fa72ca66fd1d84
Daniel Franklin <daniel@danielzfranklin.org>
2021-03-10 18:37:46 +0000

    Fix visibility

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,15 @@ use std::os::raw::c_uint;
 
 // Defined as an enum without specified values, so bindgen doesn't generate these.
 // However, we can know the values per <https://stackoverflow.com/questions/6434105/are-default-enum-values-in-c-the-same-for-all-compilers>
-const EVDI_STATUS_AVAILABLE: c_uint = 0;
-const EVDI_STATUS_UNRECOGNIZED: c_uint = 1;
-const EVDI_STATUS_NOT_PRESENT: c_uint = 2;
+pub const EVDI_STATUS_AVAILABLE: c_uint = 0;
+pub const EVDI_STATUS_UNRECOGNIZED: c_uint = 1;
+pub const EVDI_STATUS_NOT_PRESENT: c_uint = 2;
 
 include!("./bindings.rs");
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn evdi_check_device_for_not_present() {
-        let status = unsafe {
-            evdi_check_device(42)
-        };
-        assert_eq!(status, EVDI_STATUS_NOT_PRESENT)
-    }
 
     #[test]
     fn is_correct_version() {
